be7b93fabc07df7de63f42d8f6d682adcb6a033f
Antoni2021 <antonio.bond2013@gmail.com>
2021-05-08 14:05:42 +0300

    add lesson4

---
+++ src/com/hillel/lesson4/Cat.java
@@ -0,0 +1,41 @@
+package com.hillel.lesson4;
+
+public class Cat {
+
+    String name;
+    int age;
+    String color;
+
+//    public Cat(){
+//    this.color = "no";
+//    this.age = 3;
+//    this.name = "Default cat";
+//        System.out.println("Cat is created");
+//    }
+    public Cat(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString(){
+        return "Cat : " +this.name + ", " + this.color+ ", " + this.age;
+    }
+
+    public boolean equals(Object obj){
+
+        if(obj instanceof Cat){
+            Cat cat = (Cat) obj;
+            return this.name.equals(cat.name) && this.age == cat.age;
+        }
+
+        return false;
+
+    }
+
+}
