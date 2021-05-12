6171c4567e597a32104a8ce08de6f283857ac228
Anna <cijanovschi22@gmail.com>
2021-05-12 22:18:51 +0300

    commit

--- src/com/hillel/Main.java
+++ src/com/hillel/Main.java
@@ -1,7 +1,35 @@
 package com.hillel;
 
-public class Main {
-    public static void main(String[] args) {
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader t = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[5];
+        for (int i = 0; i < array.length; i++) {
+            int d = (5 - i);
+            System.out.println("Введите " + d + " любых строк: ");
+            array[i] = t.readLine();
+        }
+
+
+        System.out.println("Ваши строки: ");
+        for (String i : array) {
+            System.out.print(i + "/ ");
+
+        }
+        System.out.println( );
+        Arrays.sort(array, Comparator.comparing(String::length));
+        for (String i : array) {
+            System.out.print(i + "/ ");
+
+        }
     }
 }
+
