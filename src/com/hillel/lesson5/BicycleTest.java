15eaf8b3a0910990ac57cf2d62bc795ad1719029
Antoni2021 <antonio.bond2013@gmail.com>
2021-05-15 16:19:51 +0300

    eats

---
+++ src/com/hillel/lesson5/BicycleTest.java
@@ -0,0 +1,16 @@
+package com.hillel.lesson5;
+
+public class BicycleTest {
+    public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle("Model S");
+        bicycle.ride();
+
+        Bicycle.Seat seat = bicycle.new Seat();
+        seat.up();
+        seat.down();
+
+        Bicycle.SteeringWheel steeringWheel = bicycle.new SteeringWheel();
+        steeringWheel.left();
+        steeringWheel.right();
+    }
+}
